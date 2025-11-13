051946669e9ba7fce95d9724b974306fa41eca6b
Saad Nadeem <saadnadeemch07@gmail.com>
2025-11-14 01:00:35 +0500

    Basic Setup of Frontend Backend Done - without CORS

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch(" http://localhost:3000/api/message")
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((err) => {
+        console.log(
+          "Error fetching the data from http://localhost:3000/api/message"
+        );
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Production Code</h1>
+      <h2 className="mt-4">Data from backend: {message}</h2>
+    </>
+  );
+}
+
+export default App;
